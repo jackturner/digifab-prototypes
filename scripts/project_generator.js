2a764a0bc0fb64428e2704db063e608e12bcd645
Jack Turner <jack@turner.io>
2016-07-30 12:08:40 -0400

    Node script for building dummy data for filtering. It does this by writing out projects.json which contains fake data for any number of companies (right now I have it set to make 12 fake companies). This file will eventually be generated by the backend admin system that digifab will use.
    New /filtering folder that will explore the user interactions encountered while filtering projects.

---
+++ scripts/project_generator.js
@@ -0,0 +1,27 @@
+var faker = require('faker'),
+		fs = require('fs')
+
+var random_project = function (num) {
+	return {
+	  "name": faker.company.companyName(),
+	  "location_name": faker.fake("{{address.city}}, {{address.stateAbbr}}"),
+	  "location": faker.fake("{{address.latitude}},{{address.longitude}}"),
+	  "cost_dollars": parseInt(faker.commerce.price(8000, 500000)),
+	  "description": faker.lorem.paragraph,
+	  "thumbnail": "images/projects/" + (num+1) + ".jpg"
+	}
+}
+
+var projects = []
+
+for (var i = 0; i < 12; i++)
+	projects.push( random_project(i) )
+
+console.log(projects)
+
+fs.writeFile("../filtering/projects.json", JSON.stringify(projects, null, 2), function(err) {
+  if(err)
+    return console.log(err)
+
+  console.log("The file was saved!")
+})
