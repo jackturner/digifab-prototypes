24c714c69b407b81ae67fd8182c9c04bc54e99aa
Jack Turner <jack@turner.io>
2016-08-17 11:37:02 -0400

    Now showing relevant data in the project cards when filtering by cost or location. Now showing a map with search when going to filter by location, but this doesn't actually filter the results yet.

--- filtering/filtering.js
+++ filtering/filtering.js
@@ -1,3 +1,5 @@
+// map.getBounds().toArray()
+
 $(function() {
 
 	var $project_card_container = $('#project-cards'),
@@ -13,7 +15,9 @@ $(function() {
 				.css('background-image', 'url("' + project.thumbnail + '")')
 				.data(project)
 				.append('<div class="blur" />')
-				.append('<div class="content"><span class="name">' + project.name	 + '</span></div>')
+				.append('<div class="content"><span class="name">' + project.name	 + '</span>' + 
+								'<span class="info cost">$' + numberWithCommas(project.cost) + '</span>' + 
+								'<span class="info location">' + project.location + '</span></div>')
 				.appendTo($project_card_container)
 
 		}
@@ -26,10 +30,39 @@ $(function() {
 		var filter_name = $(this).data('filter'),
 				filter_ui = $('#filter-' + filter_name)
 
+		$project_card_container.removeClass()
 	  $project_cards.show()
 
 		$('.filter').not(filter_ui).hide()
-		filter_ui.toggle()
+		if(filter_ui.is(':visible')) {
+			filter_ui.hide()
+		} else {
+			filter_ui.show()
+			$project_card_container.addClass(filter_name)
+			try{
+				eval('show_' + filter_name + '()')
+			} catch(e) {
+				console.log(e)
+			}
+	  }
+	}
+
+	var show_cost = function() {
+		console.log('show_cost()')
+	}
+
+	var show_location = function() {
+		console.log('show_location()')
+
+    mapboxgl.accessToken = 'pk.eyJ1IjoiamFja3R1cm5lciIsImEiOiJjaXJ4a2gzcm8wMGNwMnpsZWV0cjZnMm9mIn0.6z2T5s2R2jjGHAEb2Dtm4A'
+    var map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/outdoors-v9',
+      center: [-73.8051687, 42.2512107],
+      zoom: 5
+    })
+    map.addControl(new mapboxgl.Geocoder())
+
 	}
 
 	var setup_cost_slider = function() {
@@ -44,11 +77,11 @@ $(function() {
 			}
 		})
 
-		cost_slider.noUiSlider.on('change', update_cost_filter)
+		cost_slider.noUiSlider.on('change', filter_by_cost)
 
 	}
 
-	var update_cost_filter = function(range) {
+	var filter_by_cost = function(range) {
 		$project_cards.each(function() {
 			var $this = $(this),
 					project_cost = $this.data('cost')
@@ -58,6 +91,10 @@ $(function() {
 			else
 				$this.hide()
 		})
+	}
+
+	var numberWithCommas = function(x) {
+	    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 	}
 
 	$.getJSON( "projects.json", build_projects)
